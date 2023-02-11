8d6fe4c4eb14de20d0bedf533710f44b7a3792f7
lucasXiaofan <91307310+lucasXiaofan@users.noreply.github.com>
2023-02-11 09:25:45 -0800

    bug fixing, jump attack, patrol

--- Assets/Main/Script/Enemy/EnemyStates/DP_EnemyAttackState.cs
+++ Assets/Main/Script/Enemy/EnemyStates/DP_EnemyAttackState.cs
@@ -7,6 +7,7 @@ namespace DP
     public class DP_EnemyAttackState : DP_State
     {
         public DP_EnemyPursueState pursueState;
+        
         public override DP_State Tick(DP_EnemyManger enemyManger,
                                     DP_EnemyStats enemyStats,
                                     DP_EnemyAnimator enemyAnimator,
@@ -17,7 +18,14 @@ namespace DP
                 enemyAnimator.anim.SetFloat("Vertical", 0);
                 return pursueState;
             }
+            // Hanle rotation
+            Vector3 AttackDirection = enemyLocomotion.currentTarget.transform.position- enemyManger.transform.position;
+            Quaternion targetDir = Quaternion.LookRotation(AttackDirection);
+            enemyManger.transform.rotation = Quaternion.Slerp(enemyManger.transform.rotation,targetDir,Time.deltaTime/0.1f);
+
+
             enemyManger.AttackTarget();
+
             return this;
         }
     }
